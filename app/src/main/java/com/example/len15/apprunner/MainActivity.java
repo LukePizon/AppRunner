74c6bd31ccc08f7f964f4e5fda8513e0d054afe4
Len15 <lukepizon@gmail.com>
2019-02-08 23:23:05 +0100

    added background thread

--- app/src/main/java/com/example/len15/apprunner/MainActivity.java
+++ app/src/main/java/com/example/len15/apprunner/MainActivity.java
@@ -1,5 +1,7 @@
 package com.example.len15.apprunner;
 
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.TextureView;
@@ -14,12 +16,37 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        output = (TextView) findViewById(R.id.textView);
+        output = findViewById(R.id.textView);
     }
     public void runClickHandler(View view){
-        output.append("Button clicked\n");
+        //output.append("Button clicked\n");
+        MyAsyncTask task = new MyAsyncTask();
+        task.execute("String1" , "String2", "String3");
+
     }
     public void clearClickHandler(View view) {
         output.setText("");
+    }
+
+    @SuppressLint("StaticFieldLeak")
+    private class MyAsyncTask extends AsyncTask<String, String, Void> {
+        @Override
+        protected  Void doInBackground(String... strings){
+            for (String string: strings){
+                publishProgress(string);
+                try {
+                    Thread.sleep(1000);
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onProgressUpdate(String... values){
+            super.onProgressUpdate(values);
+            output.append(values[0] + "\n");
+        }
     }
 }
