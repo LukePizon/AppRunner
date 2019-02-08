511ab73a78381baac15e0a66900cd6e71eedbdc6
Len15 <lukepizon@gmail.com>
2019-02-08 22:47:30 +0100

    Initial commit

---
+++ app/src/main/java/com/example/len15/apprunner/MainActivity.java
@@ -0,0 +1,25 @@
+package com.example.len15.apprunner;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.TextureView;
+import android.view.View;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+     TextView output;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        output = (TextView) findViewById(R.id.textView);
+    }
+    public void runClickHandler(View view){
+        output.append("Button clicked\n");
+    }
+    public void clearClickHandler(View view) {
+        output.setText("");
+    }
+}
